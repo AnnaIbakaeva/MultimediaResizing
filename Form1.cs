737fcd8da06f6a20d2b3b296262e4b110b82dfc1
Anna <annaibakaeva@gmail.com>
2016-03-14 19:48:36 +0500

    123

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResizingApplication
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private List<double> CreateInitArray(int n)
+        {
+            var initArray = new List<double>();
+            for (int i = 0; i < n; i++)
+            {
+                initArray.Add(random.NextDouble());
+            }
+            return initArray;
+        }
+        
+    }
+}
---
+++ SizeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResizingApplication
+{
+    public static class SizeConverter
+    {
+        public static List<Point> Convert(List<Point> initArray, int newCount)
+        {
+            var newArray = new List<Point>();
+            return newArray;
+        }
+
+        private static void CalculateSquare()
+        {
+
+        }
+
+        private static void CalculateEquation(Point p1, Point p2)
+        {
+        }
+    }
+}
